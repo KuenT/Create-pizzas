d402dfc50d94a9defd75f4f5e11fdc91f7b5ccd1
Quentin Tenga <quentin@MacBook-Pro-de-Quentin-2.local>
2022-04-26 19:43:53 +0200

    adding all

---
+++ templates/footer.php
@@ -0,0 +1,4 @@
+<!DOCTYPE html>
+<html>
+<div>Le footer</div>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body class="grey lighten-4">
+    <nav class="white z-depth-0">
+        <div class="container">
+            <a href="index.php" class="brand-logo brand-text">Ninja Pizzas</a>
+            <ul id="nav-mobile" class="right hide-on-small-and-down">
+                <li><a href="add.php" class="btn brand z-depth-0">Add a pizza</a></li>
+            </ul>
+        </div>
+    </nav>
+</body>
+</html>
